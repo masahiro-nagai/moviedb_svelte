8305d9aef667924bcdacbe0954164b39e28942e3
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-05-10 23:35:23 +0900

    button on click

---
+++ src/routes/about.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to About page</h1>
+<a href="/">Back to Home</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    let count = 0
+
+    function incrementBtn(){
+        count += 1
+    }
+</script>
+<h1>Number: {count}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="/about">Go to About</a>
+<button on:click={incrementBtn}>Increment</button>
+
+<style>
+    p{
+        font-size: 2rem;
+    }
+</style>
