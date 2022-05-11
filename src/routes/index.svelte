2f7f1967a7bd3372674bd7716eddacb3c3d492fd
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-05-11 23:58:22 +0900

    api error

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,6 @@
-<script>
-    let count = 0
-
-    function incrementBtn(){
-        count += 1
+<script context="module">
+    export async function load({fetch}){
+        const res = await fetch(`
+https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1`)
     }
 </script>
-<h1>Number: {count}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<a href="/about">Go to About</a>
-<button on:click={incrementBtn}>Increment</button>
-
-<style>
-    p{
-        font-size: 2rem;
-    }
-</style>
